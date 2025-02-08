5c4e27cbe4f919a8c3e47dc8b18da84db1eee5a1
DecioFrancis <francisdasilva@gmail.com>
2025-02-08 15:31:11 -0300

    fix: add files to fix bug of pio file import

---
+++ pio_matrix.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+
+#include "pio_matrix.pio.h" // Programa PIO para controle dos LEDs
+
+int main()
+{
+  stdio_init_all(); // Inicializa a comunicação serial para depuração
+
+  while (true)
+  {
+  }
+}
